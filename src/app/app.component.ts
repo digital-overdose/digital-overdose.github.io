89540bf8688c9578ef8ef9baa921ddcc98227787
AtomicNicos <nicolas.boeckh@etu.unige.ch>
2021-02-07 22:05:08 +0100

    Nu header, Home screen!

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, RouterEvent } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'digital-overdose';
+export class AppComponent implements OnInit {
+  container: boolean = true;
+  constructor(private router: Router) { }
+
+  ngOnInit(): void {
+    this.router.events.subscribe(
+      (event) => {
+        if (event instanceof RouterEvent) {
+          this.container = !['/home'].includes(event.url)
+        }
+      }
+    );
+  }
 }
--- src/app/content/common-ui/post/author-card/author-card.component.ts
+++ src/app/content/common-ui/post/author-card/author-card.component.ts
@@ -13,5 +13,4 @@ export class AuthorCardComponent implements OnInit {
 
   ngOnInit(): void {
   }
-
 }
