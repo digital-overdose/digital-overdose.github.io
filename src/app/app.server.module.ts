184675a432b17bd08433246370c8dce97a67ad16
AtomicMaya <atomicmaya@pm.me>
2021-02-11 03:18:25 +0100

    -prerender

--- src/app/app.server.module.ts
+++ src/app/app.server.module.ts
@@ -1,14 +1,25 @@
-import { NgModule } from '@angular/core';
-import { ServerModule } from '@angular/platform-server';
+import { App2021Module } from './submodules/app-content-2021.module';
+import { SharedModule } from 'src/app/shared.module';
 
-import { AppModule } from './app.module';
-import { AppComponent } from './app.component';
+import {NgModule} from '@angular/core';
+import {ServerModule, ServerTransferStateModule} from '@angular/platform-server';
+import {ModuleMapLoaderModule} from '@nguniversal/module-map-ngfactory-loader';
+
+import {AppModule} from './app.module';
+import {AppComponent} from './app.component';
 
 @NgModule({
   imports: [
+    // The AppServerModule should import your AppModule followed
+    // by the ServerModule from @angular/platform-server.
     AppModule,
+    SharedModule,
+    App2021Module,
     ServerModule,
+    ServerTransferStateModule,
   ],
+  // Since the bootstrapped component is not inherited from your
+  // imported AppModule, it needs to be repeated here.
   bootstrap: [AppComponent],
 })
 export class AppServerModule {}
