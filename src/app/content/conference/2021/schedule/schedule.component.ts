909c021044d2ee94456db82e73a93fde111538b9
AtomicNicos <atomicnicos@gmail.com>
2021-03-15 11:34:34 +0100

    Timer mode

--- src/app/content/conference/2021/schedule/schedule.component.ts
+++ src/app/content/conference/2021/schedule/schedule.component.ts
@@ -1,21 +1,36 @@
 import { Component, OnInit } from '@angular/core';
 import { BASE_URL } from 'src/app/data/url';
 import { MetaService } from 'src/app/services/meta.service';
-
+import { interval } from 'rxjs';
+import { startWith } from 'rxjs/operators';
+import { diff } from 'src/app/utils/time-until';
+import { BrowserRecognitionService } from 'src/app/services/browser-recognition.service';
 @Component({
   templateUrl: './schedule.component.html',
   styleUrls: ['../../conference.component.scss']
 })
 export class Dovercon2021ScheduleComponent implements OnInit {
+  time: number[] = [0, 0, 0, 0];
+
+  nowEpoch: number = new Date().getTime();
+  releaseEpoch: number = 1617188400000;
+
   /**
    * Creates an instance of ConferenceComponent.
    *
    * @param meta The HTML header metadata injection service.
    */
-  constructor(private meta: MetaService) { }
+  constructor(private meta: MetaService, private browserRec: BrowserRecognitionService) { }
 
   /** Set the page metadata information. */
   ngOnInit(): void {
+    if (this.browserRec.isBrowser) {
+      interval(1000).pipe(startWith(0)).subscribe(() => {
+        this.nowEpoch = new Date().getTime();
+        this.time = diff(this.releaseEpoch - this.nowEpoch);
+      });
+    }
+
     this.meta.setTags(`${BASE_URL}/dovercon/schedule-edition-2021`, 'DOVERCON2021: Schedule',
                       `${BASE_URL}/assets/images/cover.jfif`, 750, 750,
                       ['conference', 'for', 'rookies', 'schedule', 'dovercon'],
