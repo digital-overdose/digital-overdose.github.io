a238a213687c7be5dae9d4526a609f7dfdb2e421
AtomicMaya <atomicmaya@pm.me>
2021-10-12 01:21:02 +0200

    CTF end

---
+++ src/app/content/ctf/ctf2021-spring/ctf2021-spring.component.ts
@@ -0,0 +1,24 @@
+import { BASE_URL } from '../../../data/url';
+import { MetaService } from '../../../services/meta.service';
+import { Component, OnInit } from '@angular/core';
+import { sponsorsCTFSpring2021 } from 'src/app/data/ctf/2021-spring/sponsors';
+import { Mentor } from 'src/app/interfaces/mentor';
+
+@Component({
+  selector: 'app-ctf2021spring',
+  templateUrl: './ctf2021-spring.component.html',
+  styleUrls: ['../ctf.components.scss', '../../../../assets/css/team-card.scss']
+})
+export class Ctf2021SpringComponent implements OnInit {
+  sponsors: Mentor[] = sponsorsCTFSpring2021;
+
+  constructor(private meta: MetaService) { }
+
+  ngOnInit(): void {
+    this.meta.setTags(`${BASE_URL}/ctf/2021-spring`, 'Digital Overdose Conference 2021 CTF',
+                      `${BASE_URL}/assets/images/cover.jfif`, 750, 750,
+                      ['capture the flag', 'ctf', 'cybersecurity'],
+                      'Digital Overdose is running virtual Capture the Flag challenges!');
+  }
+
+}
--- src/app/submodules/app-ctf.module.ts
+++ src/app/submodules/app-ctf.module.ts
@@ -1,15 +1,15 @@
-import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { SharedModule } from 'src/app/shared.module';
-import { Ctf2021Component } from '../content/ctf/ctf2021/ctf2021.component';
 import { AboutCTFComponent } from '../content/ctf/about/about.component';
-import { CTFLogoComponent } from '../content/ctf/logo/logo.component';
-import { HeaderComponent } from '../content/ctf/header/header.component';
 import { Ctf2021AutumnComponent } from '../content/ctf/ctf2021-autumn/ctf2021-autumn.component';
+import { Ctf2021SpringComponent } from '../content/ctf/ctf2021-spring/ctf2021-spring.component';
+import { HeaderComponent } from '../content/ctf/header/header.component';
+import { CTFLogoComponent } from '../content/ctf/logo/logo.component';
 
 @NgModule({
   declarations: [
-    Ctf2021Component,
+    Ctf2021SpringComponent,
     Ctf2021AutumnComponent,
     AboutCTFComponent,
     CTFLogoComponent,
