01daeb3cd91bf78bc78bb9ea1453d3a84ad0a1b6
AtomicNicos <atomicnicos@pm.me>
2022-02-03 15:53:42 +0100

    release link

--- 9-es2015.2aebc49b5ccfc6aabd4d.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[9],{OR2H:function(n,t,o){"use strict";o.r(t),o.d(t,"Posts2021RoutingModule",function(){return u});var r=o("tyNb"),s=o("tRGF"),e=o("fXoL");const i=s.a;let u=(()=>{class n{}return n.\u0275mod=e.Gb({type:n}),n.\u0275inj=e.Fb({factory:function(t){return new(t||n)},imports:[[r.f.forChild(i)],r.f]}),n})()}}]);
