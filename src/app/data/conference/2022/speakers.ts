786e5843c71bdff88db29ed18c8a40e30a19ff48
AtomicMaya <atomicmaya@pm.me>
2022-03-11 10:44:13 +0100

    Speakers

--- src/app/data/conference/2022/speakers.ts
+++ src/app/data/conference/2022/speakers.ts
@@ -3,5 +3,489 @@ import { Speaker } from 'src/app/interfaces/speaker';
 
 /** The list of team members. */
 export const speakers: Speaker[] = [
-
+  {
+    name: 'Tony Haresign',
+    description: 'How To Present Your Security Awareness Training With Better Engagement',
+    imageUrl: '/assets/images/conference/2022/speakers/placeholder.jfif',
+    links: [
+      {
+        label: 'LinkedIn',
+        icon: 'fab fa-fw fa-linkedin',
+        url: 'https://www.linkedin.com/in/tony-haresign/',
+      },
+    ],
+  },
+  {
+    name: 'Sable',
+    description: 'My Malware Skills Are Getting Rusty',
+    imageUrl: '/assets/images/conference/2022/speakers/sable.png',
+    links: [
+      {
+        label: 'Twitter',
+        icon: 'fab fa-fw fa-twitter-square',
+        url: 'https://twitter.com/theokaydev'
+      },
+      {
+        label: 'GitLab',
+        icon: 'fab fa-fw fa-gitlab',
+        url: 'https://gitlab.com/sable',
+      }
+    ],
+  },
+  {
+    name: 'Apurv',
+    alias: ['Aiden Senju'],
+    description: 'Getting Started in Security Auditing and GRC',
+    imageUrl: '/assets/images/conference/2022/speakers/placeholder.jfif',
+    links: [
+      {
+        label: 'Website',
+        icon: 'fas fa-globe',
+        url: 'https://apurvt.github.io',
+      },
+      {
+        label: 'Twitter',
+        icon: 'fab fa-fw fa-twitter-square',
+        url: 'https://twitter.com/AidenSenju'
+      },
+      {
+        label: 'LinkedIn',
+        icon: 'fab fa-fw fa-linkedin',
+        url: 'https://www.linkedin.com/in/apurvraja/',
+      },
+    ],
+  },
+  {
+    name: 'Aditya Narayan Sinha',
+    alias: ['0x0elliot'],
+    description: 'The problem of computers and randomness',
+    imageUrl: '/assets/images/conference/2022/speakers/0x0elliot.jpg',
+    links: [
+      {
+        label: 'Website',
+        icon: 'fas fa-globe',
+        url: 'https://narayn.work',
+      },
+      {
+        label: 'Twitter',
+        icon: 'fab fa-fw fa-twitter-square',
+        url: 'https://twitter.com/0x0elliot'
+      },
+      {
+        label: 'LinkedIn',
+        icon: 'fab fa-fw fa-linkedin',
+        url: 'https://www.linkedin.com/in/adityanarayansinha/',
+      },
+    ],
+  },
+  {
+    name: 'Mark Giles',
+    alias: ['CyberCiderCity'],
+    description: 'The way forward with 2FA',
+    imageUrl: '/assets/images/conference/2022/speakers/placeholder.jfif',
+    links: [
+      {
+        label: 'Website',
+        icon: 'fas fa-globe',
+        url: 'https://herefordcomputerhelp.co.uk',
+      },
+      {
+        label: 'Twitter',
+        icon: 'fab fa-fw fa-twitter-square',
+        url: 'https://twitter.com/CyberCiderCity'
+      },
+      {
+        label: 'LinkedIn',
+        icon: 'fab fa-fw fa-linkedin',
+        url: 'https://www.linkedin.com/in/cybercidercity/',
+      },
+      {
+        label: 'Facebook',
+        icon: 'fab fa-fw fa-facebook',
+        url: 'https://www.facebook.com/mark.herefordcomputerhelp',
+      }
+    ],
+  },
+  {
+    name: 'Elliot Ownbey',
+    description: 'Seeing the Human Behind the Hack',
+    imageUrl: '/assets/images/conference/2022/speakers/placeholder.jfif',
+    links: [
+      {
+        label: 'Twitter',
+        icon: 'fab fa-fw fa-twitter-square',
+        url: 'https://twitter.com/OhNoElliot'
+      },
+      {
+        label: 'LinkedIn',
+        icon: 'fab fa-fw fa-linkedin',
+        url: 'https://www.linkedin.com/in/elliotownbey/',
+      },
+    ],
+  },
+  {
+    name: 'Squiblydoo',
+    description: 'From Novice to Noticed: a malware analyst\'s observations from being nobody to becoming somebody',
+    imageUrl: '/assets/images/conference/2022/speakers/squiblydoo.png',
+    links: [
+      {
+        label: 'Website',
+        icon: 'fas fa-globe',
+        url: 'https://www.squiblydoo.blog',
+      },
+      {
+        label: 'Twitter',
+        icon: 'fab fa-fw fa-twitter-square',
+        url: 'https://twitter.com/SquiblydooBlog'
+      },
+    ],
+  },
+  {
+    name: 'Ahmed Rashwan',
+    alias: ['Rsasec0x01'],
+    description: 'Offensive Security: Welcome To The Dark Side',
+    imageUrl: '/assets/images/conference/2022/speakers/rsasec0x01.jpeg',
+    links: [
+      {
+        label: 'Website',
+        icon: 'fas fa-globe',
+        url: 'https://ahmedsherifrashwan.github.io/',
+      },
+      {
+        label: 'Twitter',
+        icon: 'fab fa-fw fa-twitter-square',
+        url: 'https://twitter.com/AhmedRsasec0x01'
+      },
+      {
+        label: 'Instagram',
+        icon: 'fab fa-fw fa-instagram-square',
+        url: 'https://www.instagram.com/rsasec0x01/',
+      },
+      {
+        label: 'YouTube',
+        icon: 'fab fa-youtube',
+        url: 'https://www.youtube.com/channel/UCs9eGDLYFObHoBp6cH9vocA',
+      },
+    ],
+  },
+  {
+    name: 'Greg Burkland',
+    alias: ['Ghost Protocol'],
+    description: 'Hack your boss',
+    imageUrl: '/assets/images/conference/2022/speakers/ghostprotocol.jpg',
+    links: [
+      {
+        label: 'Twitter',
+        icon: 'fab fa-fw fa-twitter-square',
+        url: 'https://twitter.com/ghostprot_do'
+      },
+    ],
+  },
+  {
+    name: 'Abhinav Khanna',
+    alias: ['ak'],
+    description: 'API Pentesting - The OWASP Way!',
+    imageUrl: '/assets/images/conference/2022/speakers/placeholder.jfif',
+    links: [
+      {
+        label: 'Website',
+        icon: 'fas fa-globe',
+        url: 'https://infosecak.github.io',
+      },
+      {
+        label: 'Twitter',
+        icon: 'fab fa-fw fa-twitter-square',
+        url: 'https://twitter.com/khannaabhinav_'
+      },
+      {
+        label: 'LinkedIn',
+        icon: 'fab fa-fw fa-linkedin',
+        url: 'https://in.linkedin.com/in/abhinav-khanna-a2633b114',
+      },
+    ],
+  },
+  {
+    name: 'Chris Meistre',
+    alias: ['chrismeistre'],
+    description: 'Red Teaming - My first physical assessment',
+    imageUrl: '/assets/images/conference/2022/speakers/chrismeistre.png',
+    links: [
+      {
+        label: 'Website',
+        icon: 'fas fa-globe',
+        url: 'https://chrismeistre.github.io/',
+      },
+      {
+        label: 'Twitter',
+        icon: 'fab fa-fw fa-twitter-square',
+        url: 'https://twitter.com/chrismeistre'
+      },
+      {
+        label: 'LinkedIn',
+        icon: 'fab fa-fw fa-linkedin',
+        url: 'https://www.linkedin.com/in/chrismeistre/',
+      },
+    ],
+  },
+  {
+    name: 'Jennifer Gomez',
+    alias: ['Jen'],
+    description: 'Tangled Web of Romance Fraud',
+    imageUrl: '/assets/images/conference/2022/speakers/placeholder.jfif',
+  },
+  {
+    name: 'Denice',
+    alias: ['de3ev'],
+    description: 'The Trifecta of Email Authentication',
+    imageUrl: '/assets/images/conference/2022/speakers/de3ev.jpg',
+    links: [
+      {
+        label: 'Twitter',
+        icon: 'fab fa-fw fa-twitter-square',
+        url: 'https://twitter.com/de3ev'
+      },
+    ],
+  },
+  {
+    name: 'Patrick Rus',
+    description: 'Leveraging Telegram Bots to automate country-specific OSINT workflows',
+    imageUrl: '/assets/images/conference/2022/speakers/patrick_rus.jpg',
+    links: [
+      {
+        label: 'Twitter',
+        icon: 'fab fa-fw fa-twitter-square',
+        url: 'https://twitter.com/IntelligenceQu6'
+      },
+    ],
+  },
+  {
+    name: 'The Crabby Hacker',
+    description: 'Mind the Gap: Understanding What Your Security Control Framework Covers & Misses',
+    imageUrl: '/assets/images/conference/2022/speakers/thecrabbyhacker.jpg',
+    links: [
+      {
+        label: 'Website (Under Construction)',
+        icon: 'fas fa-globe',
+        url: 'https://thecrabbyhacker.net',
+      },
+    ],
+  },
+  {
+    name: 'Peter',
+    description: 'Automate Security Operations with Node-Red',
+    imageUrl: '/assets/images/conference/2022/speakers/peter.jfif',
+    links: [
+      {
+        label: 'LinkedIn',
+        icon: 'fab fa-fw fa-linkedin',
+        url: 'https://www.linkedin.com/in/pengchengluo/',
+      },
+    ],
+  },
+  {
+    name: 'Alexandru Lazar',
+    description: 'Helping Humanitarian NGO\'s - Perspectives from the CyberPeace Builders Program',
+    imageUrl: '/assets/images/conference/2022/speakers/AL.jpg',
+    links: [
+      {
+        label: 'LinkedIn',
+        icon: 'fab fa-fw fa-linkedin',
+        url: 'https://www.linkedin.com/in/alexandru-lazar-9a152996/',
+      },
+    ],
+  },
+  {
+    name: 'Michael Hopewell',
+    description: 'How to Scope Better for Compliance',
+    imageUrl: '/assets/images/conference/2022/speakers/michaelhopewell.jpg',
+    links: [
+      {
+        label: 'Website',
+        icon: 'fas fa-globe',
+        url: 'https://www.michaelhopewell.co.uk',
+      },
+      {
+        label: 'Twitter',
+        icon: 'fab fa-fw fa-twitter-square',
+        url: 'https://twitter.com/mqhopewell'
+      },
+      {
+        label: 'LinkedIn',
+        icon: 'fab fa-fw fa-linkedin',
+        url: 'https://www.linkedin.com/in/mqhopewell/',
+      },
+      {
+        label: 'Facebook',
+        icon: 'fab fa-fw fa-facebook',
+        url: 'https://www.facebook.com/mqhopewell/',
+      }
+    ],
+  },
+  {
+    name: 'Chloe',
+    alias: ['ErrorByNight'],
+    description: 'Hercule DFIRot: Peril at C:/Users/Me',
+    imageUrl: '/assets/images/conference/2022/speakers/errorbynight.jpg',
+    links: [
+      {
+        label: 'Twitter',
+        icon: 'fab fa-fw fa-twitter-square',
+        url: 'https://twitter.com/ErrorByNight'
+      },
+    ],
+  },
+  {
+    name: 'Doug Kras',
+    alias: ['DFreshALot'],
+    description: 'The Adventures of IoT Hacking',
+    imageUrl: '/assets/images/conference/2022/speakers/placeholder.jfif',
+    links: [
+      {
+        label: 'Website',
+        icon: 'fas fa-globe',
+        url: 'https://dfresh.ninja',
+      },
+      {
+        label: 'Twitter',
+        icon: 'fab fa-fw fa-twitter-square',
+        url: 'https://twitter.com/DFreshalot'
+      },
+      {
+        label: 'LinkedIn',
+        icon: 'fab fa-fw fa-linkedin',
+        url: 'https://www.linkedin.com/in/douglas-kras/',
+      },
+    ],
+  },
+  {
+    name: 'Arpit Nandi',
+    alias: ['AlienX'],
+    description: 'How I hacked our college as my internal testing experience',
+    imageUrl: '/assets/images/conference/2022/speakers/AlienX.jpg',
+    links: [
+      {
+        label: 'Twitter',
+        icon: 'fab fa-fw fa-twitter-square',
+        url: 'https://twitter.com/AlienX_2001'
+      },
+      {
+        label: 'LinkedIn',
+        icon: 'fab fa-fw fa-linkedin',
+        url: 'https://www.linkedin.com/in/arpit-nandi-894511206',
+      },
+    ],
+  },
+  {
+    name: 'Glen',
+    description: 'What can be hidden in QR codes',
+    imageUrl: '/assets/images/conference/2022/speakers/placeholder.jfif',
+    links: [
+    ],
+  },
+  {
+    name: 'Tori Outlaw',
+    alias: ['Bee'],
+    description: 'Cryptology: pre tech era (a short history) ',
+    imageUrl: '/assets/images/conference/2022/speakers/placeholder.jfif',
+    links: [
+      {
+        label: 'Twitter',
+        icon: 'fab fa-fw fa-twitter-square',
+        url: 'https://twitter.com/itsbeejuice'
+      },
+      {
+        label: 'LinkedIn',
+        icon: 'fab fa-fw fa-linkedin',
+        url: 'https://www.linkedin.com/in/tori-bee-outlaw/',
+      },
+      {
+        label: 'Linktr.ee',
+        icon: 'fas fa-external-link-square-alt',
+        url: 'https://linktr.ee/itsbeejuice',
+      }
+    ],
+  },
+  {
+    name: 'Danielle Arcon',
+    alias: ['Danni'],
+    description: 'Grey(Hat)\'s Anatomy: Common grounds of body immunity and cybersecurity',
+    imageUrl: '/assets/images/conference/2022/speakers/daniellearcon.jpg',
+    links: [
+      {
+        label: 'Twitter',
+        icon: 'fab fa-fw fa-twitter-square',
+        url: 'https://twitter.com/msdaniellearcon'
+      },
+      {
+        label: 'LinkedIn',
+        icon: 'fab fa-fw fa-linkedin',
+        url: 'https://www.linkedin.com/in/danielle-arcon-174579128',
+      }
+    ],
+  },
+  {
+    name: 'Tom Quinn',
+    alias: ['TomQuinn8'],
+    description: 'Foundations: Risk 101',
+    imageUrl: '/assets/images/conference/2022/speakers/tomquinn8.jpg',
+    links: [
+      {
+        label: 'Twitter',
+        icon: 'fab fa-fw fa-twitter-square',
+        url: 'https://twitter.com/TomQuinn8'
+      },
+      {
+        label: 'LinkedIn',
+        icon: 'fab fa-fw fa-linkedin',
+        url: 'https://www.linkedin.com/in/tomquinn8/',
+      },
+    ],
+  },
+  {
+    name: 'Yui',
+    description: 'Exploitation: Past to Present',
+    imageUrl: '/assets/images/conference/2022/speakers/yuictf.jpg',
+    links: [
+      {
+        label: 'Twitter',
+        icon: 'fab fa-fw fa-twitter-square',
+        url: 'https://twitter.com/YuiCTF'
+      },
+    ],
+  },
+  {
+    name: 'Stefan',
+    alias: ['CyberScriber'],
+    description: 'An Overview of the T-Pot Honeypot',
+    imageUrl: '/assets/images/conference/2022/speakers/placeholder.jfif',
+    links: [
+      {
+        label: 'Twitter',
+        icon: 'fab fa-fw fa-twitter-square',
+        url: 'https://twitter.com/CyberScriber'
+      },
+      {
+        label: 'Instagram',
+        icon: 'fab fa-fw fa-instagram-square',
+        url: 'https://www.instagram.com/thecyberscriber/',
+      },
+    ],
+  },
+  {
+    name: 'Simon Gurney',
+    description: 'Presenting our free and open-source SMB file share auditing tool',
+    imageUrl: '/assets/images/conference/2022/speakers/simon.jfif',
+    links: [
+      {
+        label: 'LinkedIn',
+        icon: 'fab fa-fw fa-linkedin',
+        url: 'https://www.linkedin.com/in/simon-gurney-2396375b/',
+      },
+      {
+        label: 'GitHub',
+        icon: 'fab fa-fw fa-github-square',
+        url: 'https://github.com/SimonGurney',
+      }
+    ],
+  },
 ];
