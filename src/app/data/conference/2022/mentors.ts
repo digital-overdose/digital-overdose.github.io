a54100fe0a4c84137006f4846c9c56898bb09dcc
AtomicNicos <nicolas.boeckh@etu.unige.ch>
2022-03-17 23:38:14 +0100

    adding people

--- src/app/data/conference/2022/mentors.ts
+++ src/app/data/conference/2022/mentors.ts
@@ -3,5 +3,20 @@ import { Mentor } from 'src/app/interfaces/mentor';
 
 /** The list of team members. */
 export const mentors: Mentor[] = [
-
+  {
+    name: 'James Bore',
+    imageUrl: '/assets/images/conference/2022/mentors/placeholder.jfif',
+    links: [
+      {
+        label: 'Twitter',
+        icon: 'fab fa-fw fa-twitter-square',
+        url: 'https://twitter.com/coffee_fueled'
+      },
+      {
+        label: 'LinkedIn',
+        icon: 'fab fa-fw fa-linkedin',
+        url: 'https://www.linkedin.com/in/lisa-forte/',
+      },
+    ]
+  },
 ];
