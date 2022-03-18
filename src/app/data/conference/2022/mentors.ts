6da42c5f05fad3df03e14f1622ec936d3d073887
AtomicNicos <atomicnicos@pm.me>
2022-03-18 07:34:53 +0100

    Adding mentors

--- src/app/data/conference/2022/mentors.ts
+++ src/app/data/conference/2022/mentors.ts
@@ -5,8 +5,13 @@ import { Mentor } from 'src/app/interfaces/mentor';
 export const mentors: Mentor[] = [
   {
     name: 'James Bore',
-    imageUrl: '/assets/images/conference/2022/mentors/placeholder.jfif',
+    imageUrl: '/assets/images/conference/2022/mentors/coffee_fueled.jpeg',
     links: [
+      {
+        label: 'Website',
+        icon: 'fas fa-globe',
+        url: 'https://bores.com',
+      },
       {
         label: 'Twitter',
         icon: 'fab fa-fw fa-twitter-square',
@@ -15,8 +20,81 @@ export const mentors: Mentor[] = [
       {
         label: 'LinkedIn',
         icon: 'fab fa-fw fa-linkedin',
-        url: 'https://www.linkedin.com/in/lisa-forte/',
+        url: 'https://www.linkedin.com/in/jbore/',
       },
+    ]
+  },
+  {
+    name: 'Bryna Kirzner',
+    imageUrl: '/assets/images/conference/2022/mentors/placeholder.jfif',
+    links: [ ]
+  },
+  {
+    name: 'Mark S.',
+    alias: ['Binary Sleeper'],
+    imageUrl: '/assets/images/conference/2022/mentors/placeholder.jfif',
+    links: [ ]
+  },
+  {
+    name: 'Lori Barfield',
+    imageUrl: '/assets/images/conference/2022/mentors/placeholder.jfif',
+    links: [ ]
+  },
+  {
+    name: 'Howard Chen',
+    imageUrl: '/assets/images/conference/2022/mentors/placeholder.jfif',
+    links: [ ]
+  },
+  {
+    name: 'Rosie Anderson',
+    imageUrl: '/assets/images/conference/2022/mentors/placeholder.jfif',
+    links: [ ]
+  },
+  {
+    name: 'Paul Love',
+    imageUrl: '/assets/images/conference/2022/mentors/placeholder.jfif',
+    links: [ ]
+  },
+  {
+    name: 'Sam Humphries',
+    imageUrl: '/assets/images/conference/2022/mentors/placeholder.jfif',
+    links: [ ]
+  },
+  {
+    name: 'Damon Small',
+    imageUrl: '/assets/images/conference/2022/mentors/placeholder.jfif',
+    links: [ ]
+  },
+  {
+    name: 'Gary Hawkins',
+    imageUrl: '/assets/images/conference/2022/mentors/gary_hawkins.jpeg',
+    links: [ ]
+  },
+  {
+    name: 'Sam Ferguson',
+    alias: ['AffineSecurity'],
+    imageUrl: '/assets/images/team/affinesecurity.png',
+    links: [
+      {
+        label: 'Website',
+        icon: 'fas fa-globe',
+        url: 'https://affinesecurity.gitlab.io/',
+      },
+      {
+        label: 'Twitter',
+        icon: 'fab fa-fw fa-twitter-square',
+        url: 'https://twitter.com/AffineSecurity'
+      },
+      {
+        label: 'LinkedIn',
+        icon: 'fab fa-fw fa-linkedin',
+        url: 'https://www.linkedin.com/in/affinesecurity/',
+      },
+      {
+        label: 'GitLab',
+        icon: 'fab fa-fw fa-gitlab',
+        url: 'https://gitlab.com/AffineSecurity',
+      }
     ]
   },
 ];
