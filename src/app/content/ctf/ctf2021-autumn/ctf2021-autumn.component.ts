630b64151bd1bb53440192377815990ec60cb0dc
AtomicMaya <atomicmaya@pm.me>
2022-04-02 01:43:53 +0200

    CTF 3 + speaker tweaks

--- src/app/content/ctf/ctf2021-autumn/ctf2021-autumn.component.ts
+++ src/app/content/ctf/ctf2021-autumn/ctf2021-autumn.component.ts
@@ -6,12 +6,12 @@ import { BrowserRecognitionService } from 'src/app/services/browser-recognition.
 import { interval } from 'rxjs';
 import { startWith } from 'rxjs/operators';
 import { diff } from 'src/app/utils/time-until';
-import { sponsorsCTFAutumn2021 } from 'src/app/data/ctf/2021-autumn/sponsors';
+import { sponsorsCTFAutumn2021 } from 'src/app/data/ctf/2021/autumn/sponsors';
 
 @Component({
   selector: 'app-ctf2021',
   templateUrl: './ctf2021-autumn.component.html',
-  styleUrls: ['../ctf.components.scss', '../../../../assets/css/team-card.scss',  './ctf2021-autumn.component.scss']
+  styleUrls: ['../ctf.components.scss', '../../../../assets/css/team-card.scss', './ctf2021-autumn.component.scss']
 })
 export class Ctf2021AutumnComponent implements OnInit {
   sponsors: Mentor[] = sponsorsCTFAutumn2021;
@@ -30,10 +30,10 @@ export class Ctf2021AutumnComponent implements OnInit {
       });
     }
 
-    this.meta.setTags(`${BASE_URL}/ctf/2021-autumn`, 'Digital Overdose Autumn 2021 CTF',
-                      `${BASE_URL}/assets/images/cover.jfif`, 750, 750,
-                      ['capture the flag', 'ctf', 'cybersecurity'],
-                      'Digital Overdose is running a virtual Capture the Flag challenge! Dates: Oct. 9th (00:00:00 GMT+2) - Oct. 10th (23:59:59 GMT+2');
+    this.meta.setTags(`${BASE_URL}/ctf/2021/autumn`, 'Digital Overdose Autumn 2021 CTF',
+      `${BASE_URL}/assets/images/cover.jfif`, 750, 750,
+      ['capture the flag', 'ctf', 'cybersecurity'],
+      'Digital Overdose is running a virtual Capture the Flag challenge! Dates: Oct. 9th (00:00:00 GMT+2) - Oct. 10th (23:59:59 GMT+2');
   }
 
 
