bc509ac1216fdc86c8acc75ac5fcb33174b0d956
AtomicNicos <atomicnicos@gmail.com>
2022-11-16 07:04:25 +0100

    CTF sponsors

--- src/app/data/ctf/2022/autumn/sponsors.ts
+++ src/app/data/ctf/2022/autumn/sponsors.ts
@@ -44,4 +44,77 @@ export const sponsorsCTFAutumn2022: Sponsor[] = [
       },
     ]
   },
+  {
+    name: 'CTGIntelligence',
+    imageUrl: '/assets/images/conference/2022/sponsors/ctgintelligence.jpg',
+    links: [
+      {
+        label: 'Website',
+        icon: 'fas fa-globe',
+        url: 'https://www.ctgintelligence.com/',
+      },
+      {
+        label: 'Twitter',
+        icon: 'fab fa-fw fa-twitter-square',
+        url: 'https://twitter.com/CtgIntelligence'
+      },
+      {
+        label: 'LinkedIn',
+        icon: 'fab fa-fw fa-linkedin',
+        url: 'https://www.linkedin.com/company/ctg-intelligence/',
+      }
+    ]
+  },
+  {
+    name: 'Trans Tech Tent',
+    imageUrl: '/assets/images/conference/2022/sponsors/trans-tech-tent.png',
+    links: [
+      {
+        label: 'Website',
+        icon: 'fas fa-globe',
+        url: 'https://www.transtechtent.com',
+      },
+      {
+        label: 'Twitter',
+        icon: 'fab fa-fw fa-twitter-square',
+        url: 'https://twitter.com/TransTechTent'
+      },
+      {
+        label: 'LinkedIn',
+        icon: 'fab fa-fw fa-linkedin',
+        url: 'https://www.linkedin.com/company/trans-tech-tent/',
+      },
+      {
+        label: 'Instagram',
+        icon: 'fab fa-fw fa-instagram-square',
+        url: 'https://www.instagram.com/TransTechTent/',
+      },
+    ]
+  },
+  {
+    name: 'PentesterLab',
+    imageUrl: '/assets/images/ctf/2022/spring/pentesterlab.png',
+    links: [
+      {
+        label: 'Website',
+        icon: 'fas fa-globe',
+        url: 'https://pentesterlab.com/'
+      },
+      {
+        label: 'Twitter',
+        icon: 'fab fa-fw fa-twitter-square',
+        url: 'https://twitter.com/pentesterlab'
+      },
+      {
+        label: 'Facebook',
+        icon: 'fab fa-fw fa-facebook-square',
+        url: 'https://www.facebook.com/pentesterlab',
+      },
+      {
+        label: 'LinkedIn',
+        icon: 'fab fa-fw fa-linkedin',
+        url: 'https://linkedin.com/company/pentesterlab',
+      },
+    ]
+  },
 ];
